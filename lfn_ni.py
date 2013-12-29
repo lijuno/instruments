2658c2961c46b179b8cf9ad597e991b9c522088e
ll3kx@pittsburgwin <lijunix@gmail.com>
2013-12-29 16:42:46 -0500

    Update lfn_ni.py

--- lfn_ni.py
+++ lfn_ni.py
@@ -11,10 +11,12 @@ import time
 import sys
 
 
-def usb6211_get(filename='', voltage_limit=0.2):
+def usb6211_get(filename='', voltage_limit=0.2, duration=5):
+    # duration: measurement duration, in s
     channel = 'Dev1/ai6'
     sampling_freq = 5e4
-    sampling_pts = 250000  # too many points will make the program fail
+    #sampling_pts = 250000  # too many points will make the program fail
+    sampling_pts = sampling_freq * duration
     daq = ni.USB6211()
     data = daq.get_voltage_ai(channel, voltage_limit, sampling_freq, sampling_pts)
 
@@ -25,49 +27,46 @@ def usb6211_get(filename='', voltage_limit=0.2):
     plt.close()  # avoid "hold on" for the next plot
     ut.write_data_n1('%s.dat' % filename, data)
     # plt.show()
-    
+
+def main(bias_list):
+    # LFN measurement with bandpass filter
+    sr570_write('FLTT 2', sr570_port)   # 6 dB bandpass filter
+    sr570_write('LFRQ 11', sr570_port)   # 10kHz upper bound
+    sr570_write('HFRQ 2', sr570_port)   # 0.3Hz lower bound
+    for ii in range(len(bias_list)):
+        sr570_write('BSLV %d' % bias_list[ii], sr570_port)   # set bias level
+        sr570_write('BSON 1', sr570_port)     # turn on bias
+        time.sleep(10)          # stabilize
+        for jj in range(4):
+            usb6211_get('Vbias%d_m%d' % (bias_list[ii], jj), voltage_limit=0.2, duration=5)       # record data
+        sr570_write('BSON 0', sr570_port)   # turn off bias
+        time.sleep(1)
+
+def dc(bias_list):
+    # DC coupled measurement
+    sr570_write('FLTT 3', sr570_port)   # 6 dB lowpass filter
+    sr570_write('LFRQ 11', sr570_port)
+    for ii in range(len(bias_list)):
+        sr570_write('BSLV %d' % bias_list[ii], sr570_port)   # set bias level
+        sr570_write('BSON 1', sr570_port)     # turn on bias
+        time.sleep(3)
+        usb6211_get('Vbias%d_DC' % bias_list[ii], voltage_limit=2, duration=2)
+        sr570_write('BSON 0', sr570_port)   # turn off bias
+        time.sleep(1)
+
 if __name__ == "__main__":
     sr570_port = 'COM6'
     if sys.argv[1] == 'main':
         # Example: python lfn_ni.py main
         # To run the main routine
         # bias_lst = [-800]
-        bias_lst = [-800, -400, -200, -100, 100, 200, 400]
+        bias_lst = [-800, -400, -200, 100, 200, 300]
+        main(bias_lst)
 
-        # First, do AC coupled measurement
-        sr570_write('FLTT 2', sr570_port)   # 6 dB bandpass filter
-        sr570_write('LFRQ 11', sr570_port)
-        sr570_write('HFRQ 2', sr570_port)
-        for ii in range(len(bias_lst)):
-            sr570_write('BSLV %d' % bias_lst[ii], sr570_port)   # set bias level
-            sr570_write('BSON 1', sr570_port)     # turn on bias
-            time.sleep(10)          # stabilize
-            for jj in range(4):
-                usb6211_get('Vbias%d_m%d' % (bias_lst[ii], jj), voltage_limit=0.2)              # record data
-            sr570_write('BSON 0', sr570_port)   # turn off bias
-            time.sleep(1)
-
-        # Second, get DC values
-        sr570_write('FLTT 3', sr570_port)   # 6 dB lowpass filter
-        sr570_write('LFRQ 11', sr570_port)
-        for ii in range(len(bias_lst)):
-            sr570_write('BSLV %d' % bias_lst[ii], sr570_port)   # set bias level
-            sr570_write('BSON 1', sr570_port)     # turn on bias
-            time.sleep(2)
-            usb6211_get('Vbias%d_DC' % bias_lst[ii], voltage_limit=5)
-            sr570_write('BSON 0', sr570_port)   # turn off bias
-            time.sleep(1)
     elif sys.argv[1] == 'dc':
-        bias_lst = [-800, -400, -200, -100, 100, 200, 400]
-        sr570_write('FLTT 3', sr570_port)   # 6 dB lowpass filter
-        sr570_write('LFRQ 11', sr570_port)
-        for ii in range(len(bias_lst)):
-            sr570_write('BSLV %d' % bias_lst[ii], sr570_port)   # set bias level
-            sr570_write('BSON 1', sr570_port)     # turn on bias
-            time.sleep(2)
-            usb6211_get('Vbias%d_DC' % bias_lst[ii], voltage_limit=5)
-            sr570_write('BSON 0', sr570_port)   # turn off bias
-            time.sleep(1)
+        bias_lst = [-800, -400, -200, 100, 200, 300]
+        dc(bias_lst)
+
     elif sys.argv[1] == 'sr570':
         # Example: python lfn_ni.py sr570
         # To issue a command to SR570
